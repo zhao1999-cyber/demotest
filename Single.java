8da48da7d035ead3443e22327d5dac08facb2bdd
zhao1999-cyber <54887850+zhao1999-cyber@users.noreply.github.com>
2019-09-17 22:00:55 +0800

    Update Single.java

--- Single.java
+++ Single.java
@@ -6,7 +6,7 @@ public static Single getInstance() {
 	return INSTANCE;
 }
 	public static void main(String[] args) {
-		// TODO 自动生成的方法存根
+		
 Single s1=Single.getInstance();
 Single s2=Single.getInstance();
 System.out.println(s1==s2);
