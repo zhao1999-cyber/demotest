08767ce23b6e0f8224bcdaad8987969137f3bca4
zhao1999-cyber <54887850+zhao1999-cyber@users.noreply.github.com>
2019-10-09 22:33:10 +0800

    Add files via upload

--- Tv.java
+++ Tv.java
@@ -1,9 +1,21 @@
+package tv;
 
-public class Tv {
+public abstract class Tv {
 
-	public static void main(String[] args) {
+	public abstract  void play(); 
 		// TODO 自动生成的方法存根
 
 	}
 
+
+public class HaierTv extends Tv{
+	public void play() {
+		System.out.println("This is HaierTv.");
+	}
+}
+
+public class HisenseTv extends Tv{
+	public void play() {
+		System.out.println("This is HisenseTv.");
+	}
 }
