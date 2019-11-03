a6c91aefedad7e06a6a690289faf05be21b2387f
zhao1999-cyber <54887850+zhao1999-cyber@users.noreply.github.com>
2019-11-03 19:31:17 +0800

    Add files via upload

--- Example1.java
+++ Example1.java
@@ -1,21 +1,21 @@
-
+//单例程序
 public class Example1 {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-MyThread myThread=new MyThread();
-myThread.start();
+MyThread mythread=new MyThread();
+mythread.run();
 while(true) {
-	System.out.println("线程1");
-	
+	System.out.println("main类的方法在运行");
 }
 	}
 }
-class MyThread extends Thread{
-	public void run() {
-		while (true) {
-			System.out.println("线程2");
+	class MyThread{
+		public void run() {
+			while(true) {
+				System.out.println("MyThread类中的方法在运行");
+			}
 		}
 	}
-}
+
 
