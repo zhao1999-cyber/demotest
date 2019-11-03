446b1cc0b6a0806958e26ba61123673dc90afd77
zhao1999-cyber <54887850+zhao1999-cyber@users.noreply.github.com>
2019-11-03 19:33:08 +0800

    Add files via upload

--- Example3.java
+++ Example3.java
@@ -1,26 +1,21 @@
-
+//实现Runnable接口
 public class Example3 {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-TaxiDriver td=new TaxiDriver();
-new Thread(td,"出租车1").start();
-new Thread(td,"出租车2").start();
-new Thread(td,"出租车3").start();
-new Thread(td,"出租车4").start();
-new Thread(td,"出租车5").start();
+MyThread mythread=new MyThread();
+Thread thread=new Thread(mythread);
+thread.start();
+while(true) {
+	System.out.println("main类的方法在运行");
+}
 	}
 
 }
-class TaxiDriver implements Runnable{
-	private int passagers=100;
+class MyThread implements Runnable{
 	public void run() {
 		while(true) {
-			if(passagers>0) {
-				Thread th =Thread.currentThread();
-				String th_name=th.getName();
-				System.out.println(th_name+"正在接送第"+passagers--+"位乘客");
-			}
+			System.out.println("MyThread类中的方法在运行");
 		}
 	}
 }
