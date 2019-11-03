821a56bad191e60a05b073d8e620f060d10f5d0a
zhao1999-cyber <54887850+zhao1999-cyber@users.noreply.github.com>
2019-11-03 19:33:59 +0800

    Add files via upload

--- Example5.java
+++ Example5.java
@@ -1,9 +1,25 @@
-public class TreadApp{
+
 public class Example5 {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-
+		TicketWindow tw=new TicketWindow();
+		new Thread(tw,"窗口1").start();
+		new Thread(tw,"窗口2").start();
+		new Thread(tw,"窗口3").start();
+		new Thread(tw,"窗口4").start();
 	}
 
+}
+class TicketWindow implements Runnable{
+	private int ticket=100;
+	public void run() {
+		while(true) {
+			if(ticket>0) {
+				Thread th=Thread.currentThread();
+				String th_name=th.getName();
+				System.out.println(th_name+"正在发售第"+ticket--+"张票");
+			}
+		}
+	}
 }
