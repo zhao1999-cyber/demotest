0f0b3bc7c741e8134399d249aaefb650609c196c
zhao1999-cyber <54887850+zhao1999-cyber@users.noreply.github.com>
2019-11-03 19:33:39 +0800

    Add files via upload

--- Example4.java
+++ Example4.java
@@ -3,23 +3,23 @@ public class Example4 {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-Teacher tr=new Teacher();
-new Thread(tr,"老师1").start();
-new Thread(tr,"老师2").start();
-new Thread(tr,"老师3").start();
-
+new TicketWindow().start();
+new TicketWindow().start();
+new TicketWindow().start();
+new TicketWindow().start();
 	}
 
 }
-class Teacher implements Runnable{
-	private int  notes=80;
+class TicketWindow extends Thread{
+	private int ticket=100;
 	public void run() {
 		while(true) {
-			if(notes>0) {
-				Thread th =Thread.currentThread();
-				String th_name=th.getName();
-				System.out.println(th_name+"正在分发第"+notes--+"本笔记");
-			}
+		if(ticket>0) {
+			Thread th=Thread.currentThread();
+			String th_name=th.getName();
+			System.out.println(th_name+"正在出售第"+ticket--+"张票。");
+			
 		}
 	}
+}
 }
