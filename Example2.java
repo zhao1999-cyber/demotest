408417a869753811d60da1c6655ea09486f00de8
zhao1999-cyber <54887850+zhao1999-cyber@users.noreply.github.com>
2019-11-11 17:08:33 +0800

    Add files via upload

--- Example2.java
+++ Example2.java
@@ -3,18 +3,44 @@ public class Example2 {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-MyThread mythread=new MyThread();
-mythread.start();
-while(true) {
-	System.out.println("main类中的方法在运行");
-}
+ System.out.println("1，添加---------");
+ add();
+ System.out.println("2, 删除---------");
+ remove();
+ System.out.println("3, 修改---------");
+ alter();
 	}
-
+public static void add() {
+	StringBuffer sb=new StringBuffer();
+	sb.append("abcde");
+	System.out.println("append添加结果："+sb);
+	sb.insert(2, "123");
+	System.out.println("insert添加结果： "+sb);
 }
-class MyThread extends Thread{
-	public void run() {
-		while(true) {
-			System.out.println("MyThread类中的方法在运行");
-		}
-	}
+public static void  remove() {
+	StringBuffer sb=new StringBuffer("abcdg");
+	sb.delete(1,5);
+	System.out.println("删除指定位置结果："+sb);
+	sb.deleteCharAt(2);
+	System.out.println("删除指定位置结果："+sb);
+	sb.delete(0,sb.length());
+	System.out.println("清空缓冲区结果："+sb);
+			}
+public static void alter() {
+	StringBuffer sb=new StringBuffer("abcde");
+	sb.setCharAt(1,'p');
+	System.out.println("修改指定位置字符结果："+sb);
+	sb.replace(1, 3,"qq");
+	System.out.println("替换指定位置字符（串）结果："+sb);
+	System.out.println("字符串翻转结果"+sb.reverse());
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
 }
